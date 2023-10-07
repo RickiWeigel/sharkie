740824fe53dfbd9dadcd90800b1b4ac7e105a5fa
Ricki W <ricki.weigel@gmx.de>
2023-10-07 13:43:29 +0200

    added background to canvas

--- models/pufferfish.class.js
+++ models/pufferfish.class.js
@@ -5,8 +5,6 @@ class pufferfish extends MoveableObject {
   constructor() {
     super().loadImage("img/2.Enemy/pufferfish/1.Swim/1.swim1.png");
     this.x = 400 + Math.floor(Math.random() * 721);
-    this.y = Math.floor(Math.random() * 481);
-    this.height = 70;
-    this.width = 70;
+    this.y = Math.floor(Math.random() * 410);
   }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,10 +1,12 @@
 class World {
   character = new Character();
+  backgroundObjects = [
+    new BackgroundObject("img/3.Background/Layers/5.Water/D1.png", 0, 0),
+  ];
   enemies = [new pufferfish(), new pufferfish(), new pufferfish()];
   ctx;
   canvas;
 
-  
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
@@ -13,17 +15,26 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // altes canvas wird gecleart
-    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width)
 
-    this.enemies.forEach(enemy => {
-      this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.height, enemy.width)
-    });
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addToMap(this.character);
 
+    this.addObjectsToMap(this.enemies);
 
     //draw wird immer wieder aufgerufen.
     let self = this;
-    requestAnimationFrame(function(){
+    requestAnimationFrame(function () {
       self.draw();
-    })
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
+    });
   }
 }
